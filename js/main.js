13cb448b78e3bfa0757d27cf34a65abe0cd0f6bc
Purukitto <purukitto@gmail.com>
2020-09-15 11:21:01 +0530

    v1 stable

--- js/main.js
+++ js/main.js
@@ -5,17 +5,21 @@ $(document).ready(function() {
         .then(data => {
             body = data[0];
             image = body.url;
-            iHeight = body.height;
-            iWidth = body.width;
-            var elem = document.getElementById('feedback-img');
-            if (image.slice(-3) == "gif")
-                elem.setAttribute("src", "./../doggo.jpg");
-            else elem.setAttribute("src", image);
-            if (iHeight > iWidth)
-                elem.classList.add("potrait");
-            else
-                elem.classList.add("landscape");
-
+            if (image) {
+                iHeight = body.height;
+                iWidth = body.width;
+                var elem = document.getElementById('feedback-img');
+                if (image.slice(-3) == "gif")
+                    elem.setAttribute("src", "./../doggo.jpg");
+                else elem.setAttribute("src", image);
+                if (iHeight > iWidth)
+                    elem.classList.add("potrait");
+                else
+                    elem.classList.add("landscape");
+            } else {
+                var elem = document.getElementById('feedback-img');
+                elem.setAttribute("alt", "I can't find my doggo friends just yet! Please reload! Woof!")
+            }
         })
         .catch(err => {
             console.error(err)
